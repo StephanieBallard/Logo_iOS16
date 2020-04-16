fab760587a2f5378b0bd664fd653d47306fe8ed7
Stephanie Ballard <user.name>
2020-04-15 17:13:53 -0700

    traffic light lights up

--- Logo_iOS16/GoLogoView.swift
+++ Logo_iOS16/GoLogoView.swift
@@ -35,3 +35,5 @@ class GoLogoView: UIView {
     }
 
 }
+
+
--- Logo_iOS16/ViewController.swift
+++ Logo_iOS16/ViewController.swift
@@ -10,17 +10,55 @@ import UIKit
 
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Oulets -
+    
+    @IBOutlet weak var stopView: StopLogoView!
+    @IBOutlet weak var slowView: SlowLogoView!
+    @IBOutlet weak var goView: GoLogoView!
+    
     // MARK: - Properties -
-    
-    @IBOutlet weak var logoView: StopLogoView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        stopView.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.stopView.alpha = 1.0
+        }
+    }
+    @IBAction func slowButtonTapped(_ sender: UIButton) {
+        slowView.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.slowView.alpha = 1.0
+        }
+    }
+    
+    @IBAction func goButtonTapped(_ sender: UIButton) {
+        goView.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.goView.alpha = 1.0
+        }
+    }
+    @IBAction func pressButtonTapped(_ sender: UIButton) {
+        trafficLight()
+    }
+    
+    func trafficLight() {
+        stopView.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.stopView.alpha = 1.0
+        }
+        slowView.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.slowView.alpha = 1.0
+        }
+        goView.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.goView.alpha = 1.0
+        }
 }
-
+}
